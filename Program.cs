dff57333b1ca42a03a4bdf52ce67f0ac66cd26da
Joshua Barton <joshdbarton@gmail.com>
2022-11-21 10:42:15 -0600

    removing extraneous writeline

--- Program.cs
+++ Program.cs
@@ -8,7 +8,6 @@ Console.WriteLine("Guess the Secret Number!");
    
 // Use a random number to set the secret number between 1 and 100 instead of a hard-coded number.
 int secretNumber = new Random().Next(1, 101);
-Console.WriteLine(secretNumber);
 
 // The prompt should display the number of guesses the user has left.
 for (int i = 4; i >= 1; i--)

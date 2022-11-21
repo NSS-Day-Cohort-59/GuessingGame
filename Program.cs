5f376f9b8978deb4876a7fb7dd5a7b842b18a310
Joshua Barton <joshdbarton@gmail.com>
2022-11-21 10:47:22 -0600

    phase six complete

--- Program.cs
+++ Program.cs
@@ -6,20 +6,21 @@
 
 Console.WriteLine("Guess the Secret Number!");
    
-// Use a random number to set the secret number between 1 and 100 instead of a hard-coded number.
 int secretNumber = new Random().Next(1, 101);
+Console.WriteLine(secretNumber);
 
-// The prompt should display the number of guesses the user has left.
 for (int i = 4; i >= 1; i--)
 {  
     Console.Write($"Your Guess (Guesses left: {i}): ");
-    string answer = Console.ReadLine();
-    if (secretNumber == int.Parse(answer)) 
+    int answer = int.Parse(Console.ReadLine());
+    if (secretNumber == answer) 
     {
         Console.WriteLine("correct!");
         break;
     }
     else {
-        Console.WriteLine("Nope!");
+
+        // Inform the user if their guess was too high or too low, when they guess incorrectly.
+        Console.WriteLine(secretNumber > answer ? "too low!" : "too high!");
     }
 }

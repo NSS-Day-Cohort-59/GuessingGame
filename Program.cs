24a3e44cbc73d324c9983bd8700ee07d7f7b0220
Joshua Barton <joshdbarton@gmail.com>
2022-11-21 10:34:05 -0600

    phase four complete

--- Program.cs
+++ Program.cs
@@ -2,18 +2,21 @@
 
 
 
+
 Console.WriteLine("Guess the Secret Number!");
    
 int secretNumber = 42;
 
-// Give the user four chances to guess the number.
 for (int i = 1; i <= 4; i++)
 {  
-    Console.Write("Your Guess: ");
+    // Display the number of the user's current guess in the prompt. For example, if the user has already guessed one time, the prommpt should say something like Your guess (2)>.
+    Console.Write($"Your Guess ({i}): ");
     string answer = Console.ReadLine();
     if (secretNumber == int.Parse(answer)) 
     {
+        // End the loop early if the user guesses the correct number.
         Console.WriteLine("correct!");
+        break;
     }
     else {
         Console.WriteLine("Nope!");

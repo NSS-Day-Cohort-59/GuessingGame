95b760d15c6fabab8d00fac89d40c3339720f6cf
Joshua Barton <joshdbarton@gmail.com>
2022-11-21 10:17:59 -0600

    phase one complete

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
 ﻿using System;
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+    // Display a message to the user asking them to guess the secret number.
+Console.WriteLine("Guess the Secret Number!");
+   
+    // Display a prompt for the user's guess.
+Console.Write("Your Guess: ");
+    // Take the user's guess as input and save it as a variable.
+string answer = Console.ReadLine();   
+    // Display the user's guess back to the screen.
+Console.WriteLine($"Your Guess: {answer}");

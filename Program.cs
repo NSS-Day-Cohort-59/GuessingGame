a678a6a61f260cf266cfaad64707c04f1597b5ea
Joshua Barton <joshdbarton@gmail.com>
2022-11-21 10:30:45 -0600

    phase three complete

--- Program.cs
+++ Program.cs
@@ -4,15 +4,18 @@
 
 Console.WriteLine("Guess the Secret Number!");
    
-    // Create a variable to contain the secret number. This number should be hard-coded for now. 42 is a nice number.
 int secretNumber = 42;
-Console.Write("Your Guess: ");
-string answer = Console.ReadLine();
-    // Compare the user's guess with the secret number. Display a success message if the guess is correct, otherwise display a failure message.
-if (secretNumber == int.Parse(answer))
-{
-    Console.WriteLine("correct!");
-}
-else {
-    Console.WriteLine("Nope!");
+
+// Give the user four chances to guess the number.
+for (int i = 1; i <= 4; i++)
+{  
+    Console.Write("Your Guess: ");
+    string answer = Console.ReadLine();
+    if (secretNumber == int.Parse(answer)) 
+    {
+        Console.WriteLine("correct!");
+    }
+    else {
+        Console.WriteLine("Nope!");
+    }
 }
